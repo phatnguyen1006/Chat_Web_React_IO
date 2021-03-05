9ec10bd3e812b367b4422b6ae15272d3ffd5fea4
P <phatnguyen876@gmail.com.vn>
2021-03-05 12:12:20 +0700

    Push test

--- client/src/useChat.js
+++ client/src/useChat.js
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import socketIOClient from "socket.io-client";
 
 const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
-const SOCKET_SERVER_URL = "http://";
+const SOCKET_SERVER_URL = "https://chat-web-react-io.vercel.app";
 
 const useChat = (roomId) => {
   const [messages, setMessages] = useState([]);
